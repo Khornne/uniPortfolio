403b121ba3ec7c0108fbe589cc27c07fdc82839f
Khornne <maurice.pattinsonn@gmail.com>
2023-10-09 22:49:22 +0100

    Finally got the while loop to work

--- Week 3/passManagerLoop.py
+++ Week 3/passManagerLoop.py
@@ -3,30 +3,34 @@ BAD_PASSWORDS = ['password', 'letmein', 'sesame', 'hello', 'justinbieber']
 
 while True:
     Password_Input = input("Choose a password: ") 
-
-    if len(Password_Input) > 8:
+    if len(Password_Input) < 8:
         print("Password must have a minimum of 8 characters")
-        break
+        continue
+
     elif len(Password_Input) > 12:
         print("Password must have a maximum of 12 characters")
-        break
+        continue
+
     elif Password_Input in BAD_PASSWORDS:
         print("Please choose a stronger password")
-        break
-    else:
-
-
-
+        continue
     
+    Password_Confirm = input("Re-enter password to confirm: ")
+    if Password_Input != Password_Confirm:
+        print("Error! Password does not match. Please try again.")
+        continue  
+    else:
+        print("Password set")
+        break
+exit(0)
 
 
-Password_Confirm = input("Re-enter password to confirm: ")
-if Password_Input == Password_Confirm:
-    print("Password set")
-    exit(0)
-
-else:
-    print("Error! Password does not match. Please try again.")
+        
+    
+    
+    
+    
+ 
 
     
 

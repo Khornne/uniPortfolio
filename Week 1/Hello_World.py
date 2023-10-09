39188d9cde8f3feea963302525c38e3ecf97e8a3
Korn <146092919+Khornne@users.noreply.github.com>
2023-10-09 15:57:45 +0100

    Week 1 Portfolio Work
    First step of learning python

---
+++ Week 1/Hello_World.py
@@ -0,0 +1,4 @@
+# My first steps
+print("Hello, World!")
+
+print("Hello, Maurice!")
---
+++ Week 1/Temperature.py
@@ -0,0 +1,3 @@
+Temp = (38.4 * 9/5) + 32
+
+print("The temperature in Yorkshire is 38.4 degrees celsius or",Temp, "in fahrenheit")

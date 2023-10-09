7de08a6b2422bb94cbee314a9207b98fe8c3ee9c
Khornne <maurice.pattinsonn@gmail.com>
2023-10-10 00:11:13 +0100

    Added multiplication table

---
+++ Week 3/sevenMultiple.py
@@ -0,0 +1,5 @@
+num = 7
+
+for n in range (1, 13):
+    print(num, "x", n, "=", num*n)
+
---
+++ Week 3/sevenMultipleReverse.py
@@ -0,0 +1,5 @@
+num = int(input("Please input a number to multiply to: "))
+
+for n in range(1, 13):
+    print(num, "x", n, "=", num*n)
+

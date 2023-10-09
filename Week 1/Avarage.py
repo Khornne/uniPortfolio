b0696f139d1e11a3b6c7af1bc06999d7132447c4
Khornne <maurice.pattinsonn@gmail.com>
2023-10-09 19:17:42 +0100

    Updated strings to f-strings

--- Week 1/Avarage.py
+++ Week 1/Avarage.py
@@ -1,4 +1,4 @@
 avarage = int(48426 / (1014 - 162))
 
-print("Boycott's batting avarage is", avarage)
+print(f"Boycott's batting avarage is {avarage}")
 
--- Week 1/Hello_Maurice.py
+++ Week 1/Hello_Maurice.py
@@ -1,2 +1,2 @@
 print("Hello, Maurice!")
-
+#HIIIIIIIII

2e284b5300319ae511c015444d870e2fe6180a66
Khornne <maurice.pattinsonn@gmail.com>
2023-10-09 17:37:48 +0100

    Rebuilt Dividing_Groups code

--- Week 1/Dividing_Groups.py
+++ Week 1/Dividing_Groups.py
@@ -1,3 +1,7 @@
-print(113 // 24)
-print(175 // 24)
-print(12 // 24)
+Students = 175
+grpSize = 24
+
+grpTotal = int(Students / grpSize)
+grpSmall = Students - (grpTotal * grpSize)
+
+print(f"The total amount of students that are in full groups are {grpTotal}, however there will be group of {grpSmall} that will be left over. ")

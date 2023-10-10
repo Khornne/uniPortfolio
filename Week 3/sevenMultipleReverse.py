c2e18d2f61c2bf208e68b4cb2474abea476f8855
Khornne <maurice.pattinsonn@gmail.com>
2023-10-10 14:28:23 +0100

    Added reverse table when integer inputed it neg

--- Week 3/sevenMultipleReverse.py
+++ Week 3/sevenMultipleReverse.py
@@ -1,5 +1,17 @@
 num = int(input("Please input a number to multiply to: "))
+limit = 12
 
-for n in range(1, 13):
+
+if num < 0:
+    for n in range(limit, 0, -1):
+        print(num, "x", n, "=", num*n)
+
+
+
+for n in range(1, 13):      
     print(num, "x", n, "=", num*n)
+
+exit(0)
+
+    
 

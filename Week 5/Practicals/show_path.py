f8c28611720d4e7c3a2d96ddbd4b61eb9b34e113
Khornne <maurice.pattinsonn@gmail.com>
2023-11-14 13:55:09 +0000

    Finished Practicals Week 5

---
+++ Week 5/Practicals/show_path.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+	pass
+
+
+import sys
+
+print("The import search path for this program is", sys.path)
---
+++ Week 5/Practicals/utils_test.py
@@ -0,0 +1,4 @@
+from my_utils import average
+
+print("Avarage is", average([10, 20, 30]))
+print("Another average is", average([10.2, 8.8, 2.6]))

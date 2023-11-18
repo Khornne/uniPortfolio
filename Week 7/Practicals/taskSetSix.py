2c8eec22010f16c1b93b644bbfe0d0aade57f734
Khornne <maurice.pattinsonn@gmail.com>
2023-11-18 00:51:45 +0000

    Finished Week 7 Practicals

---
+++ Week 7/Practicals/taskSetSix.py
@@ -0,0 +1,5 @@
+staff = set({"Pete", "Kelly", "Jon", "Paul", "Sally", "Sue"})
+managers = {"Kelly", "Jon", "Paul", "Sally", "Sue"}
+
+if staff.issuperset(managers):
+    print("All managers are staff memebers")
